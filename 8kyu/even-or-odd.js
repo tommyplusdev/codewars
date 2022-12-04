d4a3e2353c49b49c142605e91aea9977804e1b59
tommy <tsiek@pm.me>
2022-12-03 22:57:09 -0500

    feat: add solution for even-or-odd.js

--- 8kyu/even-or-odd.js
+++ 8kyu/even-or-odd.js
@@ -1,3 +1,6 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 // My Solution
+
+let parity = int => (int % 2 === 0) ? 'Even' :  'Odd';
+console.log(parity(2));
