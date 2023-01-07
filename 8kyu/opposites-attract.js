da299d287892359c90a352f1bcd0761be2d98654
tommy <tsiek@pm.me>
2023-01-06 23:24:22 -0500

    add: opposites-attract 8kyu

---
+++ 8kyu/opposites-attract.js
@@ -0,0 +1,12 @@
+//Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+    // moment of truth
+    if (flower1 % 2 === 0 && flower2 % 2 === 1) {
+      return true
+    } else if (flower1 % 2 === 1 && flower2 % 2 === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
