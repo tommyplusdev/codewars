a603e975869c7a8b78217b73fa414f4de4280f2c
tommy <tsiek@pm.me>
2023-02-04 18:24:07 -0500

    feat: add 2 answers to list-filtering.js

--- 7kyu/list-filtering.js
+++ 7kyu/list-filtering.js
@@ -8,6 +8,20 @@
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
+// without filter
 function filter_list(l) {
     // Return a new array with the strings filtered out
+    let newArray = [];
+    l.forEach(element => {
+      if (typeof element === "number") {
+        newArray.push(element)
+      }
+    })
+    return newArray;
+}
+
+// with filter
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+    return l.filter(element => typeof element === "number");
 }
