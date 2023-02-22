c72afe9e38376a05e44a051e5cd4f49fa9344953
tommy <tsiek@pm.me>
2023-02-21 22:15:17 -0500

    feat: add answer/pseudo-code to round-up-to-the-next-multiple-of-5.js

--- 7kyu/round-up-to-the-next-multiple-of-5.js
+++ 7kyu/round-up-to-the-next-multiple-of-5.js
@@ -17,5 +17,23 @@
 // You can assume that all inputs are valid integers.
 
 function roundToNext5(n) {
-    ///...
-}
+    if (n === 0) {
+      return 0;
+    } else if (n > 0) { // handle positive numbers
+      if (n % 5 === 0) {
+        return n;
+      } else {
+        return n + (5 - (n % 5));
+      }
+    } else { // handle negative numbers
+      if (n % 5 === 0) {
+        return n;
+      } else {
+        return n - (n % 5);
+      }
+    }
+  }
+  
+  // % 5 modulus should equal 0
+  // if it isn't, add the remainder's difference (5-remainder) to the number, then return the number.
+  // that way, no need to loop through numbers.
