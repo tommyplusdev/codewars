00c4648a4d485b58d20fb3b85b956ca274c30ebe
tommy <tsiek@pm.me>
2023-04-10 18:15:16 -0400

    feat: add answer/pseudo-code to duplicate-encoder.js

--- 6kyu/duplicate-encoder.js
+++ 6kyu/duplicate-encoder.js
@@ -10,6 +10,33 @@
 // Notes
 // Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 
-function duplicateEncode(word) {
-    // ...
-}
+function duplicateEncode(word){
+    let charFreq = {};
+    let result = "";
+    word = word.toLowerCase();
+    
+    // Count the frequency of each character
+    for (let i = 0; i < word.length; i++) {
+      if (charFreq[word[i]]) {
+        charFreq[word[i]]++;
+      } else {
+        charFreq[word[i]] = 1;
+      }
+    }
+    
+    // Determine whether each character is a duplicate
+    for (let i = 0; i < word.length; i++) {
+      if (charFreq[word[i]] === 1) {
+        result += "(";
+      } else {
+        result += ")";
+      }
+    }
+    
+    return result;
+  }
+  
+  
+  // store the count of the characters in an object
+  // build the string, just like in C depending on the count amount check
+  // return the string
