cb3cf31cd56cd943602eaf65524dcc23ca8a6a14
tommy <tsiek@pm.me>
2023-04-19 20:07:47 -0400

    feat: add answers/pseudo-code to count-the-smiley-faces.js

--- 6kyu/count-the-smiley-faces.js
+++ 6kyu/count-the-smiley-faces.js
@@ -19,3 +19,51 @@
 
 // Note
 // In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+
+//return the total number of smiling faces in the array
+
+// first try
+// function countSmileys(arr) {
+//   let count = 0;
+//   const eyes = new Set([':', ';']);
+//   const noses = new Set(['-', '~']);
+//   const smileyMouths = new Set(['D', ')']);
+  
+//   arr.forEach(emoji => {
+//     if (eyes.has(emoji[0]) && smileyMouths.has(emoji[emoji.length - 1])) {
+//       if (emoji.length === 2) {
+//         count++;
+//       } else if ((emoji.length === 3) && (noses.has(emoji[emoji.length - 2]))) {
+//           count++;
+//         }
+//       }
+//   });
+  
+//   return count;
+// }
+
+// second try
+function countSmileys(arr) {
+    const eyes = new Set([':', ';']);
+    const noses = new Set(['-', '~']);
+    const smileyMouths = new Set(['D', ')']);
+    
+      if (!arr || arr.length === 0) {
+      return 0;
+      }
+  
+    return arr.reduce((count, emoji) => {
+      if (emoji.length === 2 && eyes.has(emoji[0]) && smileyMouths.has(emoji[1])) {
+        return count + 1;
+      } else if (emoji.length === 3 && eyes.has(emoji[0]) && noses.has(emoji[1]) && smileyMouths.has(emoji[2])) {
+        return count + 1;
+      }
+      return count;
+    }, 0);
+  }
+  
+  
+  // array of strings that contain a smiley faces
+  // obviously check if string starts with ':' or ';'
+  // can only have one nose unfortunately
+  // check if the string ends with 'D' or ')'
