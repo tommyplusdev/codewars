fff5a4f6536486342c31e05598635703ac9090cc
tommy <tsiek@pm.me>
2023-04-27 12:54:32 -0400

    chore: initialise break-camelCase.js

---
+++ 6kyu/break-camelCase.js
@@ -0,0 +1,13 @@
+// Break camelCase
+
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+// complete the function
+function solution(string) {
+    //...
+}
