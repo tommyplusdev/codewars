c20d13cc9dc9222b0f89c056e09370a873007a32
tommy <tsiek@pm.me>
2023-04-27 12:56:02 -0400

    feat: solve break-camelCase.js

--- 6kyu/break-camelCase.js
+++ 6kyu/break-camelCase.js
@@ -9,5 +9,16 @@
 
 // complete the function
 function solution(string) {
-    //...
+    let newString = "";
+    for (let i = 0; i < string.length; i++) {
+      if (string[i]  === string[i].toUpperCase() && i !== 0) {
+        newString += " ";
+      }
+    newString += string[i];
+    }
+  return newString;
 }
+
+solution("camelCase");
+
+// build a new string with checks, C style.
