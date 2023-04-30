68674d067a07eb75e33528a665a36c14de4d18c5
tommy <tsiek@pm.me>
2023-04-30 15:33:05 -0400

    feat: add .js + .ts answer to split-strings.ts

--- 6kyu-ts/split-strings.ts
+++ 6kyu-ts/split-strings.ts
@@ -6,3 +6,27 @@
 
 // * 'abc' =>  ['ab', 'c_']
 // * 'abcdef' => ['ab', 'cd', 'ef']
+
+// JavaScript
+// function solution(str){
+//   if (str.length % 2 !== 0) {
+//     str += "_"; // append _ if it's not even
+//   }
+//   let strArray = [];
+//   for (i = 0; i < str.length; i += 2) {
+//     strArray.push(str.slice(i, i + 2))
+//   }
+//   return strArray;
+// }
+
+// TypeScript
+export function solution(str : string) : string [] {
+    if (str.length % 2 !== 0) {
+      str += "_"; // append _ if it's not even
+    }
+    let strArray: string [] = [];
+    for (let i = 0; i < str.length; i += 2) {
+      strArray.push(str.slice(i, i + 2))
+    }
+    return strArray;
+  }
