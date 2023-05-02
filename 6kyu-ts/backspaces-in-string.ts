0ddc32ac7642ebf0bbb236d6c9cc9b30873cba65
tommy <tsiek@pm.me>
2023-05-02 15:30:39 -0400

    feat: add answer to backspaces-in-string.ts

--- 6kyu-ts/backspaces-in-string.ts
+++ 6kyu-ts/backspaces-in-string.ts
@@ -12,4 +12,19 @@
 
 export function cleanString(s: string): string {
 	// ... your code ...
+  let stringArray : string [] = s.split('');
+  for (let i : number = 0; i < s.length; i++) {
+    if (stringArray[i] === "#") { // delete #
+    stringArray.splice(i, 1)
+      i--
+      if (stringArray[i]) {
+        stringArray.splice(i, 1); // delete character before if exists
+        i--
+      }
+    }
+  }
+  return stringArray.join('');
 }
+
+// everytime a character is deleted, decrement the index to
+// take into account the new array length.
