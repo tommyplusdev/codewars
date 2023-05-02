118078fee1b26aecbc68279aef0541d24d52d9ec
tommy <tsiek@pm.me>
2023-05-02 15:30:09 -0400

    initialise backspaces-in-string.ts

---
+++ 6kyu-ts/backspaces-in-string.ts
@@ -0,0 +1,15 @@
+// Backspaces in string
+
+// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+// Your task is to process a string with "#" symbols.
+
+// Examples
+// "abc#d##c"      ==>  "ac"
+// "abc##d######"  ==>  ""
+// "#######"       ==>  ""
+// ""              ==>  ""
+
+export function cleanString(s: string): string {
+	// ... your code ...
+}
