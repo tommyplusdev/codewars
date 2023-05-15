2687ceef9e88c9c7b00fb981bcaf286f641f4746
tommy <tsiek@pm.me>
2023-05-15 13:15:32 -0700

    feat: add answer to base64-encoding.ts

--- 5kyu-ts/base64-encoding.ts
+++ 5kyu-ts/base64-encoding.ts
@@ -7,3 +7,21 @@
 // You can learn more about Base64 encoding and decoding here.
 
 // Note: This kata uses the non-padding version ("=" is not added to the end).
+
+export function toBase64(str: string): string {
+    // Encode this string to Base64
+    const encoder = new TextEncoder();
+    const data: Uint8Array = encoder.encode(str); // Use 'str' instead of 'string'
+    const binaryString: string = String.fromCharCode.apply(null, Array.from(data)); // Convert Uint8Array to array
+    const base64String: string = btoa(binaryString);
+    return base64String;
+  }
+  
+  export function fromBase64(str: string): string {
+    // Decode this string from Base64
+    const decodedData: string = atob(str); // Use 'str' instead of 'string'
+    const decoder = new TextDecoder();
+    const decodedString: string = decoder.decode(new Uint8Array([...decodedData].map((c) => c.charCodeAt(0))));
+    return decodedString;
+  }
+  
--- 6kyu-ts/consonant-value.ts
+++ 6kyu-ts/consonant-value.ts
@@ -14,3 +14,63 @@
 // -- The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. The highest is 57.
 
 // For C: do not mutate input.
+
+export function solve(s: string) {
+    // your code here
+    const letterValues: { [key: string]: number } = {
+    'a': 0,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 0,
+    'f': 6,
+    'g': 7,
+    'h': 8,
+    'i': 0,
+    'j': 10,
+    'k': 11,
+    'l': 12,
+    'm': 13,
+    'n': 14,
+    'o': 0,
+    'p': 16,
+    'q': 17,
+    'r': 18,
+    's': 19,
+    't': 20,
+    'u': 0,
+    'v': 22,
+    'w': 23,
+    'x': 24,
+    'y': 25,
+    'z': 26
+  };
+    
+    const consonantSubstringScores : { [key : string] : number } = {};
+    let consonantSubstring : string = "";
+    
+    function calculateScore(string : string) : number {
+      let score : number = 0;
+      for (let i : number = 0; i < string.length; i++) {
+        score += letterValues[string[i]]; 
+      }
+      return score;
+    }
+    
+    // get substrings and set a value for their consonant substring.
+    for (let i : number = 0; i < s.length; i++) {
+      if (letterValues[s[i]] !== 0) {
+        consonantSubstring += s[i]; // add character
+      } else {
+        consonantSubstringScores[consonantSubstring] = calculateScore(consonantSubstring);
+        consonantSubstring = ""; // reset
+      }
+    }
+    
+    // if last letter happens to be a consonant...
+    if (consonantSubstring !== "") {
+    consonantSubstringScores[consonantSubstring] = calculateScore(consonantSubstring);
+    }
+    
+    return Math.max(...Object.values(consonantSubstringScores)); // return max value
+}
